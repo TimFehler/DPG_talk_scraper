43d0d5449a0e0f7db460a63b6f0f9080cb820c22
Tim Fehler <126073245+TimFehler@users.noreply.github.com>
2025-02-11 17:35:03 +0100

    Updates to HTML layout and proper sorting of table

--- talk_scraper.py
+++ talk_scraper.py
@@ -5,7 +5,8 @@ import yaml
 
 class DPG_talk:
     def __init__(self):
-        self.session_id = None
+        self.section = None # e.g. "T", "ST"
+        self.session_id = None # e.g. "23.5", "50.8"
         self.title = None
         self.authors = []
         self.presenting_author = None
@@ -15,7 +16,11 @@ class DPG_talk:
         return f'({self.session_id}): {self.title} by {self.presenting_author}'
 
     def from_HTML(self, table_row: str):
-        self.session_id = table_row.find_all('td', {"class": "scip"})[2].text
+        id_string = table_row.find_all('td', {"class": "scip"})[2].text
+        
+        # Split the ID string, e.g. "T 46.7" into section "T" and session ID "46.7"
+        self.section, self.session_id = id_string.split()
+
         self.title = table_row.find_all('a')[1].text
         self.link = table_row.find_all('a')[1]['href']
         self.authors = [x.text for x in table_row.find_all('span', {"style": "font-variant:small-caps"})]
@@ -29,6 +34,7 @@ class DPG_talk:
     def to_dict(self):
         """Convert object to dictionary for YAML serialization."""
         return {
+            'section': self.section,
             'session_id': self.session_id,
             'title': self.title,
             'authors': self.authors,
@@ -59,9 +65,8 @@ class HTML_page:
         return talks
 
 # Sorting function for session IDs
-def sort_key(talk):
-    _, value = talk.session_id.split()  # Extract "23.5", "50.8", etc.
-    major, minor = map(float, value.split("."))  # Split "23.5" into 23 and 5
+def sort_key(session_id):
+    major, minor = map(float, session_id.split("."))  # Split "23.5" into 23 and 5
     return (major, minor)
 
 
@@ -100,8 +105,8 @@ if __name__ == '__main__':
 
         print(f'Found {len(talks)} unique talks')
 
-        # Sort by session ID
-        talks = sorted(talks, key=sort_key)
+        # Sort by section, if identical, by session ID
+        talks = sorted(talks, key=lambda t: (t.section, sort_key(t.session_id)))
 
         talks_dict = [t.to_dict() for t in talks]
 
